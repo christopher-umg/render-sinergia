b9c0be32e80b13700cce488e4ba69fb15ec6d108
Christopher Taquiej <christophertaquiej@gmail.com>
2023-10-24 17:15:10 -0600

    change config

--- sinergia/settings.py
+++ sinergia/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-4ez*ojzdsbntsxh7*sp%n6(wg7aodyhpe!dzr1*lr9hh$e80z('
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = 'django-insecure-4ez*ojzdsbntsxh7*sp%n6(wg7aodyhpe!dzr1*lr9hh$e80z('
+# SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['*']
