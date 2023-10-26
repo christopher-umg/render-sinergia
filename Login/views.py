6317833781a2f9b1f942d22c775c9ea6c01c640d
Christopher Taquiej <christophertaquiej@gmail.com>
2023-10-26 05:15:10 -0600

    media

--- Login/views.py
+++ Login/views.py
@@ -73,7 +73,7 @@ def SingUpPage(request):
                 print(e)
                 traceback.print_exc()
                 return render(request, 'Pages/SignUp.html', {
-                    "error": e
+                    "error": "Usuario existente en la BDD"
                 })
         return render(request,'Pages/SignUp.html',{
             "error": "Contraseñas no coinciden"
